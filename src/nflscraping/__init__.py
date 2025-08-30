f75f6aa55c1c122da33da4c1dba3cc857f655f51
Josh Gorton <jgorton1337@gmail.com>
2025-08-30 15:44:59 -0400

    chore: bump version to 0.1.1 for PyPI release

--- src/nflscraping/__init__.py
+++ src/nflscraping/__init__.py
@@ -1,6 +1,6 @@
 # Public package interface
 from .api import get_all_player_stats, get_all_team_stats
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ["get_all_player_stats", "get_all_team_stats", "__version__"]
